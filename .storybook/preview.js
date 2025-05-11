f59da62f2016e4b7a254de786de9d90a29ed89fa
Wesley Smits <wesleysmits@icloud.com>
2025-05-11 18:19:06 +0300

    feat: add global styles, typography, and fontsource dependencies for Space Grotesk and Work Sans

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,11 @@
-import "../styles/tokens.css";
+import "@fontsource/space-grotesk/400.css";
+import "@fontsource/space-grotesk/500.css";
+import "@fontsource/space-grotesk/700.css";
+import "@fontsource/work-sans/400.css";
+import "@fontsource/work-sans/500.css";
+import "@fontsource/work-sans/700.css";
+
+import "../styles/global.css";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
