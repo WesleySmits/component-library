8d4e51a2cf8ac08e1a49e0d2b63609c772741d51
Wesley Smits <wesleysmits@icloud.com>
2025-05-12 09:12:48 +0300

    feat: add Icon component stories for Storybook integration

---
+++ src/components/atoms/icon/Icon.stories.ts
@@ -0,0 +1,38 @@
+import Icon from "./Icon.vue";
+import type { Meta, StoryFn } from "@storybook/vue3";
+import type { IconProps } from "./Icon.type";
+
+export default {
+  title: "Components/Icon",
+  component: Icon,
+  argTypes: {
+    icon: { control: "text" },
+    size: { control: "text" },
+  },
+} as Meta<typeof Icon>;
+
+const Template: StoryFn<typeof Icon> = (args: IconProps) => ({
+  components: { Icon },
+  setup() {
+    return { args };
+  },
+  template: '<Icon v-bind="args" />',
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  icon: "check",
+  size: "1x",
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  icon: "check",
+  size: "2x",
+};
+
+export const CustomIcon = Template.bind({});
+CustomIcon.args = {
+  icon: "external",
+  size: "lg",
+};
